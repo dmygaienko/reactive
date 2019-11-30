40ee1bf8dd7d1f111c0f3f7fbe49e59fdc2cfda1
dmytro.mygaienko <dmygaenko@gmail.com>
2019-11-30 20:50:27 +0200

    reactive aerospike

--- src/main/java/com/mygaienko/reactive/repo/entity/Car.java
+++ src/main/java/com/mygaienko/reactive/repo/entity/Car.java
@@ -10,7 +10,7 @@ import org.springframework.data.annotation.Id;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document
+@Document(collection = "car")
 @Builder(toBuilder = true)
 public class Car {
 
